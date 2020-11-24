e822e56d9ac6c5b5a60f8f4b9ee3e9d58eff8438
Anton Petkov <petkov.anton.o@gmail.com>
2020-11-24 23:18:12 +0300

    all data points added

--- main.py
+++ main.py
@@ -42,11 +42,57 @@ def get_data(html):
             body = ''
         print(u'Body: {}'.format(body.encode('iso-8859-1').decode('utf-8', 'ignore')))
 
+        try:
+            brand = card.find('a', {'class':'Link ListingItemTitle-module__link'})['href'].split('/')[-4]
+        except:
+            brand = ''
+        print(u'Brand: {}'.format(brand.encode('iso-8859-1').decode('utf-8', 'ignore')))
+
+        try:
+            model = card.find('a', {'class':'Link ListingItemTitle-module__link'})['href'].split('/')[-3]
+        except:
+            model = ''
+        print(u'Model: {}'.format(model.encode('iso-8859-1').decode('utf-8', 'ignore')))
+
+        try:
+            drive = card.find_all('div', {'class': 'ListingItemTechSummaryDesktop__cell'})[3].get_text(strip=True)
+        except:
+            drive = ''
+        print(u'Drive: {}'.format(drive.encode('iso-8859-1').decode('utf-8', 'ignore')))
+
+        try:
+            color = card.find_all('div', {'class': 'ListingItemTechSummaryDesktop__cell'})[4].get_text(strip=True)
+        except:
+            color = ''
+        print(u'Color: {}'.format(color.encode('iso-8859-1').decode('utf-8', 'ignore')))
+
+        try:
+            price = card.find('div', {'class': 'ListingItemPrice-module__content'}).get_text(strip=True)
+        except:
+            price = ''
+        print(u'Price: {}'.format(price.encode('iso-8859-1').decode('utf-8', 'ignore')))
+
+        try:
+            year = card.find('div', {'class': 'ListingItem-module__year'}).get_text(strip=True)
+        except:
+            year = ''
+        print(u'Year: {}'.format(year.encode('iso-8859-1').decode('utf-8', 'ignore')))
+
+        try:
+            mileage = card.find('div', {'class': 'ListingItem-module__kmAge'}).get_text(strip=True)
+        except:
+            mileage = ''
+        print(u'Mileage: {}'.format(mileage.encode('iso-8859-1').decode('utf-8', 'ignore')))
+
+
+        print('\n')
+
 
 
 def main():
-    url = 'https://auto.ru/moskva/cars/used/?page=500'
-    get_data(get_html(url))
+    for i in range(100, 110):
+        url = 'https://auto.ru/moskva/cars/used/?page={}'.format(i)
+        get_data(get_html(url))
     
 
 main()
